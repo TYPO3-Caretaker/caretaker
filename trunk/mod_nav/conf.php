773d792ece58f2a9943e809a9a7990ef0bc24a0e
etobi.de <etobi.de@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-09-22 14:24:41 +0000

    * fix #4727: replace icons for the backend module * fix #4727: the main module has no label
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/caretaker/trunk@24697 735d13b6-9817-0410-8766-e36946ffe9aa

--- trunk/mod_nav/conf.php
+++ trunk/mod_nav/conf.php
@@ -3,11 +3,11 @@
 	// DO NOT REMOVE OR CHANGE THESE 3 LINES:
 define('TYPO3_MOD_PATH', '../typo3conf/ext/caretaker/mod_nav/');
 $BACK_PATH='../../../../typo3/';
-$MCONF['name'] = 'txcaretakerNav';
+$MCONF['name']='txcaretakerNav';
 
 $MCONF['access'] = 'user,group';
 $MCONF['script'] = 'index.php';
 $MCONF['navFrameScript'] = 'index.php';
 $MCONF['default']['tabs_images']['tab'] = 'moduleicon.gif';
-$MCONF['default']['ll_ref'] = 'LLL:EXT:caretaker/mod_nav/locallang_mod.xml';
+$MLANG['default']['ll_ref']='LLL:EXT:caretaker/mod_nav/locallang_mod.xml';
 ?>
