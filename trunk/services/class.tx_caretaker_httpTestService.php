0673e441398e103028536e56c087dbabd1a4604b
martoro <martoro@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-11-18 16:29:13 +0000

    Added http-header check to the http-test. Fixes #5416
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/caretaker/trunk@26699 735d13b6-9817-0410-8766-e36946ffe9aa

--- trunk/services/class.tx_caretaker_httpTestService.php
+++ trunk/services/class.tx_caretaker_httpTestService.php
@@ -62,12 +62,13 @@ class tx_caretaker_httpTestService extends tx_caretaker_TestServiceBase {
 	 */
 	function runTest() {
 		
-		$time_warning    = $this->getTimeWarning();
-		$time_error      = $this->getTimeError();
-		$expected_status = $this->getExpectedReturnCode();
-		$request_query   = $this->getRequestQuery();
-		$request_method  = $this->getRequestMethod();
-		$request_data    = $this->getRequestData();
+		$time_warning     = $this->getTimeWarning();
+		$time_error       = $this->getTimeError();
+		$expected_status  = $this->getExpectedReturnCode();
+		$expected_headers = $this->getExpectedHeaders();
+		$request_query    = $this->getRequestQuery();
+		$request_method   = $this->getRequestMethod();
+		$request_data     = $this->getRequestData();
 		
 		$url           = $this->getInstanceUrl();
 		$request_url   = $url.$request_query;
@@ -78,9 +79,8 @@ class tx_caretaker_httpTestService extends tx_caretaker_TestServiceBase {
 		}
 
 			// execute query
-		list ($http_status, $time, $response) = $this->executeCurlRequest($request_url, $time_error, $request_method, $request_data);
-		$info_array = array('values'=>array('url'=>$request_url, 'status'=>$http_status, 'expected' => $expected_status ) );
-
+		list ($time, $response, $info, $headers) = $this->executeCurlRequest($request_url, $time_error, $request_method, $request_data);
+		$info_array = array('values'=>array('url'=>$request_url, 'status'=>$info['http_code'], 'expected' => $expected_status ) );
 
 			// ERROR
 		if ($time_error && $time > $time_error ){
@@ -92,13 +92,41 @@ class tx_caretaker_httpTestService extends tx_caretaker_TestServiceBase {
 			return tx_caretaker_TestResult::create( TX_CARETAKER_STATE_WARNING, $time ,  'LLL:EXT:caretaker/locallang_fe.xml:http_info', $info_array );
 		}
 
-			// OK
-		if ($http_status != $expected_status){
+			// OK but status fails
+		if ($info['http_code'] != $expected_status){
 			return tx_caretaker_TestResult::create( TX_CARETAKER_STATE_ERROR, $time , 'LLL:EXT:caretaker/locallang_fe.xml:http_error', $info_array  );
+		}
+
+			// OK but header fails
+		if ( !$this->checkHeaders( $expected_headers,$headers ) ){
+			return tx_caretaker_TestResult::create( TX_CARETAKER_STATE_ERROR, $time , 'LLL:EXT:caretaker/locallang_fe.xml:header_error', $info_array  );
 		}
 
 			// ERROR wrong status code
 		return tx_caretaker_TestResult::create( TX_CARETAKER_STATE_OK, $time ,  'LLL:EXT:caretaker/locallang_fe.xml:http_info', $info_array );
+		
+	}
+
+	/**
+	 *
+	 * @return boolean
+	 */
+	protected function checkHeaders($expectedHeaders,$responseHeaders){
+		$result = TRUE;
+		foreach ( $expectedHeaders as $headerName => $expectedValue ){
+				// header was found
+			if ($responseHeaders[$headerName]) {
+				if ($responseHeaders[$headerName] != $expectedValue){
+					$result = FALSE;
+				}
+			}
+				// header was not found
+			else {
+				$result = FALSE;
+			}
+		}
+
+		return $result;
 		
 	}
 	
@@ -107,7 +135,7 @@ class tx_caretaker_httpTestService extends tx_caretaker_TestServiceBase {
 	 * @return integer
 	 */
 	protected function getTimeWarning(){
-		return $this->getConfigValue('max_time_warning');
+		return intval($this->getConfigValue('max_time_warning'));
 	}
 	
 	/**
@@ -115,17 +143,38 @@ class tx_caretaker_httpTestService extends tx_caretaker_TestServiceBase {
 	 * @return integer
 	 */
 	protected function getTimeError(){
-		return $this->getConfigValue('max_time_error');
+		return intval($this->getConfigValue('max_time_error'));
 	}
 	
 	/**
-	 * 
+	 * Get the expected http status code from the test configuration
 	 * @return integer
 	 */
 	protected function getExpectedReturnCode(){
-		return $this->getConfigValue('expected_status');
+		return intval($this->getConfigValue('expected_status'));
 	}
-	
+
+	/**
+	 * Get the expected headers from the test configuration
+	 * @return array an associative Array with headers as key and expectec values as sting value
+	 */
+	protected function getExpectedHeaders(){
+		$expectedHeaders = array();
+		$expectedHeadersConfiguration = $this->getConfigValue('expected_headers');
+		if ($expectedHeadersConfiguration){
+			$configurationLines = explode (chr(10), $expectedHeadersConfiguration);
+			foreach ($configurationLines as $configurationLine){
+				list ($headerName, $headerValue) = explode (':',$configurationLine, 2);
+				$headerName  = trim($headerName);
+				$headerValue = trim($headerValue);
+				if ($headerName && $headerValue) {
+					$expectedHeaders[$headerName]= $headerValue;
+				}
+			}
+		}
+		return $expectedHeaders;
+	}
+
 	/**
 	 * 
 	 * @return string
@@ -192,17 +241,34 @@ class tx_caretaker_httpTestService extends tx_caretaker_TestServiceBase {
 				break;				
 		}
 		
-		curl_setopt($curl, CURLOPT_HEADER, 0);
-		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-		
+		curl_setopt($curl, CURLOPT_HEADER, TRUE);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+
+			// execute request
 		$response = curl_exec($curl);
 		$info     = curl_getinfo($curl);
 		curl_close($curl);
-			
+
+		list ($headerText, $content) = preg_split( '/\n[\s]*\n/', $response, 2);
+
+			// split headers
+		$headers = array();
+		if ($headerText){
+			$headerLines = explode (chr(10), $headerText);
+			foreach ($headerLines as $headerLine){
+				list ($headerName, $headerValue) = explode (':',$headerLine, 2);
+				$headerName  = trim($headerName);
+				$headerValue = trim($headerValue);
+				if ($headerName && $headerValue) {
+					$headers[$headerName]= $headerValue;
+				}
+			}
+		}
+
 		$endtime=microtime(TRUE);
 		$time = ($endtime-$starttime)*1000;
 		
-		return array ( $info['http_code'], $time ,$response);
+		return array ( $time, $response, $info, $headers );
 	}
 }
 
