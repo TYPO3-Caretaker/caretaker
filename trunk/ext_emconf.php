f10f0aee009a1c59f92bbbf9127a6f598fcfdba0
martoro <martoro@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-12-16 08:22:24 +0000

    Added suggestions for css_styled_content and caretaker_instance to the caretaker extension.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/caretaker/trunk@27763 735d13b6-9817-0410-8766-e36946ffe9aa

--- trunk/ext_emconf.php
+++ trunk/ext_emconf.php
@@ -42,6 +42,8 @@ $EM_CONF[$_EXTKEY] = array(
 		'conflicts' => array(
 		),
 		'suggests' => array(
+			'css_styled_content' => '',
+			'caretaker_instance' => ''
 		),
 	),
 	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"9c48";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:19:"doc/wizard_form.dat";s:4:"c49e";s:20:"doc/wizard_form.html";s:4:"c565";}',
