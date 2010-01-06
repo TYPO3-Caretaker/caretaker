fc0d3a845787f73713b6a60972ab394b5fa651b5
martoro <martoro@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-01-06 09:46:28 +0000

    some more streamlining of file comments and svn:keywords id
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/caretaker/trunk@28437 735d13b6-9817-0410-8766-e36946ffe9aa

--- trunk/ext_localconf.php
+++ trunk/ext_localconf.php
@@ -1,4 +1,40 @@
 <?php
+
+/***************************************************************
+* Copyright notice
+*
+* (c) 2009 by n@work GmbH and networkteam GmbH
+*
+* All rights reserved
+*
+* This script is part of the Caretaker project. The Caretaker project
+* is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* The GNU General Public License can be found at
+* http://www.gnu.org/copyleft/gpl.html.
+*
+* This script is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+/**
+ * This is a file of the caretaker project.
+ * http://forge.typo3.org/projects/show/extension-caretaker
+ *
+ * Project sponsored by:
+ * n@work GmbH - http://www.work.de
+ * networkteam GmbH - http://www.networkteam.com/
+ *
+ * $Id$
+ */
+
 if (TYPO3_MODE=='BE')    {
     $TYPO3_CONF_VARS['SC_OPTIONS']['GLOBAL']['cliKeys'][$_EXTKEY] = array('EXT:'.$_EXTKEY.'/classes/class.tx_caretaker_Cli.php','_CLI_caretaker');
 }
