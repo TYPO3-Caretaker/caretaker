5901364f89b9824afc6d9463da6ee93308dd9814
martoro <martoro@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-01-03 14:40:56 +0000

    optimized drawing of charts:
     * persist selected month in backend module
    
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/caretaker/trunk@41735 735d13b6-9817-0410-8766-e36946ffe9aa

--- trunk/res/js/tx.caretaker.NodeCharts.js
+++ trunk/res/js/tx.caretaker.NodeCharts.js
@@ -1,9 +1,59 @@
 Ext.namespace('tx','tx.caretaker');
 
+tx.caretaker.NodeChartsStatefulTabs = Ext.extend(Ext.TabPanel, {
+    stateEvents: ['tabchange'],
+    getState: function() {return{tab:this.getActiveTab().id}},
+    applyState: function(state) {this.setActiveTab(state.tab);}
+});
+
 tx.caretaker.NodeCharts = Ext.extend(Ext.Panel, {
 
     constructor: function(config) {
 
+		var tabpanel = new tx.caretaker.NodeChartsStatefulTabs (
+			{
+				xtype		: 'tabpanel',
+				id			: 'tx-caretaker-panel-nodecharts-charts',
+				region		: 'south',
+				height      : 430,
+				activeTab	: 3,
+				items: [
+					{
+						title:'1 h',
+						autoLoad  : config.back_path + 'ajax.php?ajaxID=tx_caretaker::nodegraph&node=' + config.node_id +  '&duration= ' +  (60*60)
+					},{
+						title:'3 h',
+						autoLoad  : config.back_path + 'ajax.php?ajaxID=tx_caretaker::nodegraph&node=' + config.node_id + '&duration= ' +  (60*60*3)
+					},{
+						title:'12 h',
+						autoLoad  : config.back_path + 'ajax.php?ajaxID=tx_caretaker::nodegraph&node=' + config.node_id + '&duration= ' +  (60*60*12)
+					},{
+						title:'24 h',
+						autoLoad  : config.back_path + 'ajax.php?ajaxID=tx_caretaker::nodegraph&node=' + config.node_id + '&duration= ' +  (60*60*24)
+					},{
+						title:'48 h',
+						autoLoad  : config.back_path + 'ajax.php?ajaxID=tx_caretaker::nodegraph&node=' + config.node_id + '&duration= ' +  (60*60*48)
+					},{
+						title:'7 Days',
+						autoLoad  : config.back_path + 'ajax.php?ajaxID=tx_caretaker::nodegraph&node=' + config.node_id + '&duration= ' +  (60*60*24*7)
+					},{
+						title:'1 Month',
+						autoLoad  : config.back_path + 'ajax.php?ajaxID=tx_caretaker::nodegraph&node=' + config.node_id + '&duration= ' +  (60*60*24*31)
+					},{
+						title:'3 Months',
+						autoLoad  : config.back_path + 'ajax.php?ajaxID=tx_caretaker::nodegraph&node=' + config.node_id + '&duration= ' +  (60*60*34*93)
+					},{
+						title:'6 Months',
+						autoLoad  : config.back_path + 'ajax.php?ajaxID=tx_caretaker::nodegraph&node=' + config.node_id + '&duration= ' +  (60*60*34*182)
+					},{
+						title:'12 Months',
+						autoLoad  : config.back_path + 'ajax.php?ajaxID=tx_caretaker::nodegraph&node=' + config.node_id + '&duration= ' +  (60*60*34*365)
+					}
+				]
+
+			}
+		);
+		
 		config = Ext.apply({
 			iconCls: 'tx-caretaker-panel-nodecharts',
 	    	enableTabScroll : true,
@@ -11,50 +61,12 @@ tx.caretaker.NodeCharts = Ext.extend(Ext.Panel, {
 			collapsed       : true,
 			autoHeight      : true,
 			stateful        : true,
-			stateEvents     : ['expand','collapse','tabchange'],
+			stateEvents     : ['expand','collapse'],
 			stateID         : 'tx.caretaker.NodeCharts',
+			id              : 'tx-caretaker-panel-nodecharts',
 			title           : "Charts",
 			titleCollapse   : true,
-			items:[
-				{
-					xtype		: 'tabpanel',
-					region		: 'south',
-					height      : 430,
-					activeTab	: 3,
-
-					items: [{
-							title:'1 h',
-							autoLoad  : config.back_path + 'ajax.php?ajaxID=tx_caretaker::nodegraph&node=' + config.node_id +  '&duration= ' +  (60*60)
-						},{
-							title:'3 h',
-							autoLoad  : config.back_path + 'ajax.php?ajaxID=tx_caretaker::nodegraph&node=' + config.node_id + '&duration= ' +  (60*60*3)
-						},{
-							title:'12 h',
-							autoLoad  : config.back_path + 'ajax.php?ajaxID=tx_caretaker::nodegraph&node=' + config.node_id + '&duration= ' +  (60*60*12)
-						},{
-							title:'24 h',
-							autoLoad  : config.back_path + 'ajax.php?ajaxID=tx_caretaker::nodegraph&node=' + config.node_id + '&duration= ' +  (60*60*24)
-						},{
-							title:'48 h',
-							autoLoad  : config.back_path + 'ajax.php?ajaxID=tx_caretaker::nodegraph&node=' + config.node_id + '&duration= ' +  (60*60*48)
-						},{
-							title:'7 Days',
-							autoLoad  : config.back_path + 'ajax.php?ajaxID=tx_caretaker::nodegraph&node=' + config.node_id + '&duration= ' +  (60*60*24*7)
-						},{
-							title:'1 Month',
-							autoLoad  : config.back_path + 'ajax.php?ajaxID=tx_caretaker::nodegraph&node=' + config.node_id + '&duration= ' +  (60*60*24*31)
-						},{
-							title:'3 Months',
-							autoLoad  : config.back_path + 'ajax.php?ajaxID=tx_caretaker::nodegraph&node=' + config.node_id + '&duration= ' +  (60*60*34*93)
-						},{
-							title:'6 Months',
-							autoLoad  : config.back_path + 'ajax.php?ajaxID=tx_caretaker::nodegraph&node=' + config.node_id + '&duration= ' +  (60*60*34*182)
-						},{
-							title:'12 Months',
-							autoLoad  : config.back_path + 'ajax.php?ajaxID=tx_caretaker::nodegraph&node=' + config.node_id + '&duration= ' +  (60*60*34*365)
-						}
-				]}
-			]
+			items:[ tabpanel ]
 		}, config);
 
 		tx.caretaker.NodeCharts.superclass.constructor.call(this, config);
