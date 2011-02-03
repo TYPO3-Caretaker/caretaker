4530408bd04bd514890d7d2f81fb93a3a83a9c13
matrikz <matrikz@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-02-03 11:47:29 +0000

    * [caretaker] Touched almost every PHP file and streamlined the comment on top of every file
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/caretaker/trunk@43023 735d13b6-9817-0410-8766-e36946ffe9aa

--- trunk/classes/eid/class.tx_caretaker_Eid.php
+++ trunk/classes/eid/class.tx_caretaker_Eid.php
@@ -1,27 +1,27 @@
 <?php
 /***************************************************************
-* Copyright notice
-*
-* (c) 2009 by n@work GmbH and networkteam GmbH
-*
-* All rights reserved
-*
-* This script is part of the Caretaker project. The Caretaker project
-* is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation; either version 2 of the License, or
-* (at your option) any later version.
-*
-* The GNU General Public License can be found at
-* http://www.gnu.org/copyleft/gpl.html.
-*
-* This script is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
+ * Copyright notice
+ *
+ * (c) 2009-2011 by n@work GmbH and networkteam GmbH
+ *
+ * All rights reserved
+ *
+ * This script is part of the Caretaker project. The Caretaker project
+ * is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * The GNU General Public License can be found at
+ * http://www.gnu.org/copyleft/gpl.html.
+ *
+ * This script is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
 
 /**
  * This is a file of the caretaker project.
@@ -31,7 +31,7 @@
  * n@work GmbH - http://www.work.de
  * networkteam GmbH - http://www.networkteam.com/
  *
- * $Id$
+ * $Id: class.tx_caretaker_NodeInfo.php 41726 2011-01-03 12:16:11Z martoro $
  */
 
 /**
--- trunk/ext_conf_include.php
+++ trunk/ext_conf_include.php
@@ -1,27 +1,27 @@
 <?php
 /***************************************************************
-* Copyright notice
-*
-* (c) 2009-2010 by n@work GmbH and networkteam GmbH
-*
-* All rights reserved
-*
-* This script is part of the Caretaker project. The Caretaker project
-* is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation; either version 2 of the License, or
-* (at your option) any later version.
-*
-* The GNU General Public License can be found at
-* http://www.gnu.org/copyleft/gpl.html.
-*
-* This script is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
+ * Copyright notice
+ *
+ * (c) 2009-2011 by n@work GmbH and networkteam GmbH
+ *
+ * All rights reserved
+ *
+ * This script is part of the Caretaker project. The Caretaker project
+ * is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * The GNU General Public License can be found at
+ * http://www.gnu.org/copyleft/gpl.html.
+ *
+ * This script is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
 
 /**
  * This is a file of the caretaker project.
@@ -30,8 +30,6 @@
  * Project sponsored by:
  * n@work GmbH - http://www.work.de
  * networkteam GmbH - http://www.networkteam.com/
- * 
- * @author Thomas Hempel <thomas@work.de>
  *
  * $Id$
  */
