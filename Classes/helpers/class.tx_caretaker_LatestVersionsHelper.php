78c13518e0be8cf63865c5f94fb80308ba6f8380
Tobias Liebig <work@etobi.de>
2017-07-15 19:03:41 +0200

    [TASK] refactor latest version calc and add basic tests
    replaces PR #76

--- Classes/helpers/class.tx_caretaker_LatestVersionsHelper.php
+++ Classes/helpers/class.tx_caretaker_LatestVersionsHelper.php
@@ -53,6 +53,7 @@ class tx_caretaker_LatestVersionsHelper
 
     /**
      * @return bool
+     * @throws Exception
      */
     public static function updateLatestTypo3VersionRegistry()
     {
@@ -65,52 +66,14 @@ class tx_caretaker_LatestVersionsHelper
             );
         }
 
-        $max = array();
-        $stable = array();
-		$security = array();
-		$latestLts = $releases['latest_lts'];
-        foreach ($releases as $major => $details) {
-            if (is_array($details) && !empty($details['latest'])) {
-                $max[$major] = $details['latest'];
-            }
+        $latestVersions = self::getLatestFromReleases($releases);
 
-            if (is_array($details) && !empty($details['stable'])) {
-                $stable[$major] = $details['stable'];
-            }
-            if (is_array($details) && is_array($details['releases'])) {
-			    $found = false;
-			    $latestCheckedVersion = '';
-			    foreach ($details['releases'] as $version => $versionDetails) {
-			        $latestCheckedVersion = $version;
-                    // if major version > latest_lts check if bugfix part of version number == '0';
-                    // in that case this is the security update and stop searching
-                    if (version_compare($major, $latestLts, '>')) {
-                        if ($versionDetails['type'] === 'security') {
-                            $security[$major] = $version;
-                            $found = true;
-                            break;
-                        }
-                        // if versionDetails->type === 'security' then this is the security update; stop searching
-                        if (self::extractBugfixNumberFromVersion($version) === 0) {
-                            $security[$major] = $version;
-                            $found = true;
-                            break;
-                        }
-                        // if versionDetails->type === 'security' then this is the security update; stop searching
-                    } elseif ($versionDetails['type'] === 'security') {
-                        $security[$major] = $version;
-                        $found = true;
-                        break;
-                    }
-                }
-                if (!$found) {
-			        $security[$major] = $latestCheckedVersion;
-                }
-            }
-        }
-        \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\CMS\Core\Registry')->set('tx_caretaker', 'TYPO3versions', $max);
-        \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\CMS\Core\Registry')->set('tx_caretaker', 'TYPO3versionsStable', $stable);
-		\TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\CMS\Core\Registry')->set('tx_caretaker', 'TYPO3versionsSecurity', $security);
+        /** @var \TYPO3\CMS\Core\Registry $registry */
+        $registry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\CMS\Core\Registry');
+        $registry->set('tx_caretaker', 'TYPO3versions', $latestVersions['max']);
+        $registry->set('tx_caretaker', 'TYPO3versionsStable', $latestVersions['stable']);
+        $registry->set('tx_caretaker', 'TYPO3versionsSecurity', $latestVersions['security']);
+
         return true;
     }
 
@@ -147,17 +110,37 @@ class tx_caretaker_LatestVersionsHelper
     }
 
     /**
-     * extracts the bugfix part of a version number
-     *
-     * @param string $version
-     * @return integer
+     * @param array $releases
+     * @return array
      */
-    protected static function extractBugfixNumberFromVersion($version)
+    public static function getLatestFromReleases($releases)
     {
-        $version = str_replace(array('_', ',', '-', '+'), '.', $version);
-        $version = preg_replace('/(\d)([^\d\.])/', '$1.$2', $version);
-        $version = preg_replace('/([^\d\.])(\d)/', '$1.$2', $version);
-        $versionParts = \TYPO3\CMS\Core\Utility\GeneralUtility::intExplode('.', $version, true, 3);
-        return $versionParts[2];
-	}
+        $max = array();
+        $stable = array();
+        $security = array();
+        foreach ($releases as $major => $details) {
+            if (is_array($details) && !empty($details['latest'])) {
+                $max[$major] = $details['latest'];
+            }
+
+            if (is_array($details) && !empty($details['stable'])) {
+                $stable[$major] = $details['stable'];
+            }
+            if (is_array($details) && is_array($details['releases'])) {
+                foreach ($details['releases'] as $version => $versionDetails) {
+                    $security[$major] = $version;
+                    if ($versionDetails['type'] === 'security') {
+                        $security[$major] = $version;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return array(
+                'max' => $max,
+                'stable' => $stable,
+                'security' => $security,
+        );
+    }
 }
